f19538d14c1f2482d41dd8755ee6be596a0b47c0
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-09-20 10:03:18 +0900

    Feat: Node.js 교과서 ch. 4 바탕 연습 예시 코드
    ch. 4 - 3 쿠키
    ch. 4 - 4  https, http2
    ch. 4 - 5 cluster

--- 220919_self_0915_NODE/4_3_session.js
+++ 220919_self_0915_NODE/4_3_session.js
@@ -12,6 +12,8 @@ const parseCookies = (cookie = '') =>
             return acc;
         }, {});
 
+const session = {};
+
 http.createServer(async (req, res) => {
     const cookies = parseCookies(req.headers.cookie);
 
@@ -22,14 +24,22 @@ http.createServer(async (req, res) => {
         const expires = new Date();
         // 쿠키 유효 시간을 현재 시간 + 5분으로 설정
         expires.setMinutes(expires.getMinutes() + 5);
+        
+        // 저번 예시와 달라진 부분 - 이름 -> uniqueInt, session 작성
+        const uniqueInt = Date.now();
+        session[uniqueInt] = {
+            name,
+            expires,
+        };
+
         res.writeHead(302, {
             Location: '/',
-            'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            'Set-Cookie': `session=${uniqueInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
         });
         res.end();
 
-    // name이라는 쿠키가 있는 경우
-    } else if (cookies.name) {
+    // 세션 쿠기가 존재하고, 만료 기간이 지나지 않았다면
+    } else if (cookies.session && session[cookies.session].expires > new Date()) {
         res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
         res.end(`${cookies.name}님 안녕하세요`);
     } else {
@@ -43,6 +53,6 @@ http.createServer(async (req, res) => {
         }
     }
 })
-    .listen(8084, () => {
-        console.log('8084번 포트에서 서버 대기 중입니다!');
+    .listen(8085, () => {
+        console.log('8085번 포트에서 서버 대기 중입니다!');
     });
